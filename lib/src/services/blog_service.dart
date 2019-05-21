143a351e23fd5d3c6eca3f856fc9bfefdfc17d1a
adi gunawan <adi_gunawan@live.com>
2019-05-21 19:05:10 +0800

    mint is crash

--- lib/src/services/blog_service.dart
+++ lib/src/services/blog_service.dart
@@ -1,26 +1,63 @@
 import 'package:http/io_client.dart';
+import 'package:nativeblog/src/helpers/client.dart';
 import 'package:nativeblog/src/models/article.dart';
 import 'package:googleapis/blogger/v3.dart';
 
 abstract class PostProvider<T> {
-  IOClient client;
-  Future<List<Article>> loadArticle();
+  IOClient get client;
+  String get cursor;
+  Map<String, List<int>> _storage;  // cached article is here
+  List<Article> get articles;
 
-  Article article(T raw);
+  Future<List<String>> labels();
+
+  Future<List<Article>> loadPage(String cursor, {int total}) {
+    // get total cursor we need to load
+    // for each cursor load data into the article
+  }
+
+  Article mapToArticle(T raw);
+
+  Future<List<Article>> nextPage(String cursor);
+
+  Future<List<Article>> previewPage(String cursor);
+
+  Future<List<Article>> fetchToProvider() {
+    final List<Post> results = [];
+    var pageToken;
+
+    for (var pageIndex = 1; pageIndex <= index; pageIndex++) {
+      if (pages.containsKey(pageIndex)) {
+        pageToken = pages[pageIndex].nextPageToken;
+      } else {
+        pages[pageIndex] = await blog.posts.list(blogId,
+            fetchImages: true,
+            fetchBodies: true,
+            labels: currentLabel,
+            pageToken: pageToken);
+      }
+      results.addAll(pages[pageIndex].items);
+    }
+    return results;
+  }
+
+  Future<void> clearAll();
 }
 
 class BloggerProvider implements PostProvider<Post> {
   // remove
   final String token;
+  BloggerApi _blog;
 
-  BloggerProvider(this.token);
+  BloggerProvider(this.token) {
+    _blog = BloggerApi(this.client);
+  }
 
   @override
-  IOClient client;
-
+  IOClient get client => ApiClient(parameter: {token: this.token});
 
   @override
-  Article article(Post raw) {
+  Article mapToArticle(Post raw) {
     return Article(
         title: raw.title,
         author: raw.author.displayName ?? '',
